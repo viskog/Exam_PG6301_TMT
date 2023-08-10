0f37914c777cc905817d561207405c7b7188e57c
viskog <89581174+viskog@users.noreply.github.com>
2023-08-11 01:10:46 +0200

    Added some pre defined users in the database

--- server/server.js
+++ server/server.js
@@ -12,21 +12,40 @@ const app = express();
 
 app.use(bodyParser.json());
 
+async function seedUsers(db) {
+    const usersCount = await db.collection('users').countDocuments();
+    if (usersCount === 0) {
+        const predefinedUsers = [
+            { username: 'employee1', password: 'test123', role: 'employee' },
+            { username: 'employee2', password: 'test123', role: 'employee' },
+            { username: 'manager1', password: 'test123', role: 'manager' },
+            { username: 'manager2', password: 'test123', role: 'manager' }
+        ];
+
+        await db.collection('users').insertMany(predefinedUsers);
+        console.log("Seeded predefined users.");
+    }
+}
 
 const mongoClient = new MongoClient(process.env.MONGODB_URL);
 mongoClient.connect().then(async () => {
     console.log("Connected to mongodb");
-    app.use("/api/activities", ActivitiesApi(mongoClient.db("pg6301-exam-viskog")));
-    app.use("/api/loggedHours", LoggedHoursApi(mongoClient.db("pg6301-exam-viskog")));
+    const db = mongoClient.db("pg6301-exam-viskog");
+
+    await seedUsers(db);  // Seed users if needed
+
+    app.use("/api/activities", ActivitiesApi(db));
+    app.use("/api/loggedHours", LoggedHoursApi(db));
+
     if (process.env.DEVELOPER_MODE) {
         const sampleActivities = [
             { title: 'Activity 1' },
             { title: 'Activity 2' },
         ];
-        await mongoDatabase.collection("activities").insertMany(sampleActivities);
+        await db.collection("activities").insertMany(sampleActivities);
         console.log("Sample activities inserted in developer mode");
     }
-})
+});
 
 app.use(express.static("../client/dist/"));
 
@@ -40,4 +59,4 @@ app.use((req, res, next) => {
 
 const server = app.listen(process.env.PORT || 3001, () => {
     console.log(`Started on http://localhost:${server.address().port}`)
-})
+});
