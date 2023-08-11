a05d2b542040f0078f679ac2810e8c9d4535a32d
viskog <89581174+viskog@users.noreply.github.com>
2023-08-11 03:31:19 +0200

    try

--- server/server.js
+++ server/server.js
@@ -57,6 +57,6 @@ app.use((req, res, next) => {
     }
 });
 
-const server = app.listen(process.env.PORT || 3001, () => {
+const server = app.listen(process.env.PORT || 3000, () => {
     console.log(`Started on http://localhost:${server.address().port}`)
 });
